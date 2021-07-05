c078df7fd8e25a536e89983641a64e14460e13cf
Lvv.me <cntrump@gmail.com>
2021-07-05 15:44:31 +0800

    [SwiftYogaKit] Merge YGLayoutExtensions

--- YogaKit/SwiftYogaKit/UIView+Swizzling.swift
+++ YogaKit/SwiftYogaKit/UIView+Swizzling.swift
@@ -1,10 +1,9 @@
-//
-//  UIView+Swizzling.swift
-//  SwiftYogaKit
-//
-//  Created by v on 2020/9/27.
-//  Copyright © 2020 facebook. All rights reserved.
-//
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #if os(macOS)
 import AppKit
